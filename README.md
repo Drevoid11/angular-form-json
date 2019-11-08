b6257a4ae45e2b9377c1642ee5183b7641ec59ca
Drevoid11 <paranjape.chinmay@outlook.com>
2019-11-08 14:57:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-form-json
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-form-json)
---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import {User} from './user';
+import { EnrollmentService } from './enrollment.service';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+  topics=['Angular','React','Vue'];
+
+  topicHasError=true;
+
+  userModel= new User('', 'rob@test.com',9975126721, 'default', 'morning');
+
+  constructor(private _enrollmentService: EnrollmentService){}
+
+  validateTopic(value){
+   if(value==='default'){
+    this.topicHasError=true; 
+  } else{
+    this.topicHasError=false;
+  }
+}
+
+  onSubmit(){
+    console.log(this.userModel);
+    this._enrollmentService.enroll(this.userModel).subscribe(
+      data => console.log('Success', data),
+      error => console.log('error', error),
+    )
+  }
+}
